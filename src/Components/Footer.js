e93a85a99b75fcdd519163405f0cb837c0304a22
AbdulMunam-2002 <abdulmunam666@gmail.com>
2023-11-22 17:44:59 +0500

    4th Commit

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,11 +1,26 @@
-export default function Footer()
-{
-    return
-    (
-        <>
-        <div className={"bg-dark text-white d-flex align-items-center justify-content-center pt-7"}>
-        All copy rights are reserved by Sukkur IBA University 
-        </div>
-        </>
-    )
+import React from 'react';
+import image from '../assets/images/IBA logo.png';
+
+function Footer() {
+  const containerStyle = {
+    padding: '20px',
+    background: '#343a40',
+  };
+
+  return (
+    <>
+      <div style={containerStyle} className="text-white d-flex align-items-center justify-content-center mt-5">
+        <img
+          src={image} // Replace with the actual path to your image
+          alt="Your Image"
+          width="50"
+          height="50"
+          className="mr-2" // Added margin to separate the image from text
+        />
+        All copyrights are reserved by Sukkur IBA University
+      </div>
+    </>
+  );
 }
+
+export default Footer;
